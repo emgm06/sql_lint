5f7509283fd5f4e088c4168addfc231def51f974
Ewan McFarlane <ewan2@hotmail.co.uk>
2026-01-08 18:28:03 +0000

    add new sql col

--- car_3_copy.sql
+++ car_3_copy.sql
@@ -1,6 +1,7 @@
 SELECT
     best_table.type,
-    best_table.transmission
+    best_table.transmission,
+    best_table.break
 FROM my_other_car_table AS best_table
 
 WHERE best_table.type != 'Skoda'
